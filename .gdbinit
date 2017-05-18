60196079ed2a2a5011ffd27b3470e35230afe36f
kazuki kikuchi <ery.eden@gmail.com>
2017-05-19 08:46:45 +0900

    mod CMake

---
+++ .gdbinit
@@ -0,0 +1,6 @@
+# commands from .gdbinit
+target remote localhost:3333
+load
+break main
+continue
+quit
---
+++ gdb_dbg.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+/opt/env/gcc-arm-none-eabi-5_4-2016q3/bin/arm-none-eabi-gdb $1 -x .gdbinit
